3b98c169d27e2dd84ad2e298767e475ce8260081
JorgeGWD <jorge.rodriguez.r@gmail.com>
2024-05-31 17:47:28 -0500

    Add component logic to Step5

--- src/app/components/steps/Step5.tsx
+++ src/app/components/steps/Step5.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
 
-const Step5 = () => {
-    return (
-        <div>Step5</div>
-    )
+interface StepProps {
+  prevStep: () => void;
 }
 
-export default Step5
+const Step5: React.FC<StepProps> = ({ prevStep }) => {
+    const handleSubmit = async () => {
+        const data = {
+        /* Collect all the data from previous steps */
+        };
+        const response = await fetch('API_URL', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+        });
+        if (response.ok) {
+        // Redirect to summary page
+        } else {
+        // Handle error
+        }
+    };
+
+    return (
+        <div>
+        <h2 className="text-2xl mb-4">Summary</h2>
+        {/* Display summary of all data */}
+        <button onClick={prevStep} className="p-2 bg-gray-500 text-white">Previous</button>
+        <button onClick={handleSubmit} className="p-2 bg-blue-500 text-white">Send </button>
+        </div>
+    );
+};
+
+export default Step5;
